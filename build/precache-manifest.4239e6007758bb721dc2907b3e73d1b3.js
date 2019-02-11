31d7e10f29c092b4ae87230fb274682110d5dfe8
gabba196 <g_arusoae@outlook.com>
2019-02-11 20:18:33 +0000

    First commit

---
+++ build/precache-manifest.4239e6007758bb721dc2907b3e73d1b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "040fbec0d3749da7c50b",
+    "url": "/static/js/main.040fbec0.chunk.js"
+  },
+  {
+    "revision": "b89bec06ff32c6ca712b",
+    "url": "/static/js/1.b89bec06.chunk.js"
+  },
+  {
+    "revision": "040fbec0d3749da7c50b",
+    "url": "/static/css/main.d3f20012.chunk.css"
+  },
+  {
+    "revision": "b89bec06ff32c6ca712b",
+    "url": "/static/css/1.2002fd1e.chunk.css"
+  },
+  {
+    "revision": "42286f85e2e81d40153eead8f1588598",
+    "url": "/index.html"
+  }
+];
